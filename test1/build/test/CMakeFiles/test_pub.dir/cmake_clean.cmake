ad84c3a1e3d1f482f057a92bc9d8916d955e49db
SiChuchen <m17609384279@163.com>
2022-06-01 09:50:02 +0800

    hello world program

---
+++ test1/build/test/CMakeFiles/test_pub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_pub.dir/src/test_pub.cpp.o"
+  "/home/ros/test1/test1/devel/lib/test/test_pub.pdb"
+  "/home/ros/test1/test1/devel/lib/test/test_pub"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_pub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
